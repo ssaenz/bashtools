f9059186427d7552b281c93403b6cc7b7698f9bc
ssaenz <ssaenz010@gmail.com>
2014-09-18 12:05:27 +0200

    Added Util.sh script for util utilities.

---
+++ util.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+main () {
+
+	if [ "$#" -eq 0 ]
+		then
+			showUsage;
+			exit;
+	fi
+	
+	if [ "$1" == "generate" ]; then
+		if [ -z $2 ]; then
+			showUsage;
+			exit;
+		fi
+		
+		PATH=$3
+		
+		if [ "$2" == "script" ]; then
+			if [ -z $3 ]; then
+				echo "empty name supplied, the script will be created with name 'script.sh' in";
+				pwd
+				PATH=script.sh;
+			fi
+			printScriptTemplate > $PATH;
+			exit;
+		fi
+		
+		if [ "$2" == "project" ]; then
+			echo ".... comming soon. Not yet implemented"
+			exit;
+		fi
+	fi
+}
+
+function showUsage {
+		echo ""
+		echo "ERROR: You must supply at less one argument"
+		echo ""
+		echo "Usage:   util [command] [option]"
+		echo ""
+		echo "Commands:"
+		echo ""
+		echo "	generate:			generate different files or directories:"
+		echo "		Options:"
+		echo "				script	[path]		Generate and empty script.sh with basics in the given directory"
+		echo "				project	[name]		Generate a directory with a new eclipse and several util subdirectories"
+}
+
+function printScriptTemplate {
+	echo "#!/bin/bash"
+	echo ""
+	echo "main () {"
+	echo ""
+	echo "	if [ \"\$#\" -eq 0 ]"
+	echo "		then"
+	echo "			showUsage;"
+	echo "			exit;"
+	echo "	fi"
+	echo "}"
+	echo ""
+	echo "function showUsage {"
+	echo "	echo \"Your script usage description\""
+	echo "}"
+	echo ""
+	echo "main \$@;"
+	echo ""
+}
+
+main $@
